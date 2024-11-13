c1796fa9c96ba5edb4a64160021fe5068576dbfe
shlomoShalem <s0552288304@gmail.com>
2024-11-13 11:52:42 +0200

    enum location and organization dane

---
+++ src/models/types/enums/locations.ts
@@ -0,0 +1,8 @@
+enum LOCATIONS {
+    North = 'North',
+    South = 'South',
+    Center = 'Center',
+    West_Bank = 'West_Bank'
+}
+
+export default LOCATIONS
---
+++ src/models/types/enums/organizations.ts
@@ -0,0 +1,9 @@
+enum ORGANIZATION {
+    IDF = 'IDF',
+    Hezbollah = 'Hezbollah',
+    Hamas = 'Hamas',
+    IRGC = 'IRGC',
+    Houthis = 'Houthis',
+}
+
+export default ORGANIZATION
